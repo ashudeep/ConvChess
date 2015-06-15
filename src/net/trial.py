924812cf9e5ad00b497905b18efe38762e45d56a
Ashudeep Singh <ashudeep21@gmail.com>
2015-06-15 19:55:54 +0530

    fixes

--- src/net/trial.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-import numpy as np
-data = np.load("/data/ConvChess/data/train_data/small_data.pkl.npy")
-labels = np.load("/data/ConvChess/data/train_data/small_labels.pkl.npy")
-import caffe
-solver = caffe.SGDSolver ("solverPIECE.prototxt")
-caffe.set_mode_gpu()
-solver.net.set_input_arrays (data,labels )
