2a69019b10a28e6f5c50a438372ed9b230ba96cd
Ashudeep Singh <ashudeep21@gmail.com>
2015-06-27 05:02:40 +0530

    added feature to continue

--- src/net/regression_train.py
+++ src/net/regression_train.py
@@ -3,12 +3,18 @@ from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD
 from keras.utils.io_utils import HDF5Matrix
-
+from keras.callbacks import ModelCheckpoint
+import keras
+import cPickle as pkl
+import sys
 import argparse
+sys.setrecursionlimit(40000)
 parser = argparse.ArgumentParser(description='Train Regression model using Keras')
 parser.add_argument('-r', type=float, default=0.8 , help='Ratio in which we need to split the data')
-parser.add_argument('-f', type=str, help='H5 File Name')
+parser.add_argument('-f', type=str, help='HDF5 File Name')
 parser.add_argument('--name', type=str, help='Model Name')
+parser.add_argument('-n', type=int, help='number of epocs', default=1)
+parser.add_argument('--cont', type=str, help='model weights to start training from.', default='')
 args = parser.parse_args()
 
 
@@ -19,25 +25,27 @@ def decide_split(h5_file, ratio=0.8):
 	training_size = int(0.8*size)
 	return (0,training_size, training_size+1, size)
 
-model = Sequential()
-model.add(Convolution2D(32, 6, 3, 3)) 
-model.add(Activation('tanh'))
-model.add(Convolution2D(64, 32, 3, 3))
-model.add(Activation('tanh'))
+if not args.cont:
+	model = Sequential()
+	model.add(Convolution2D(32, 6, 3, 3), ) 
+	model.add(Activation('tanh'))
+	model.add(Convolution2D(64, 32, 3, 3))
+	model.add(Activation('tanh'))
 
-model.add(Convolution2D(256, 64, 3, 3)) 
-model.add(Activation('tanh'))
+	model.add(Convolution2D(256, 64, 3, 3)) 
+	model.add(Activation('tanh'))
 
-model.add(Flatten())
-model.add(Dense(1024, 256))
-model.add(Activation('tanh'))
-#model.add(Dropout(0.5))
+	model.add(Flatten())
+	model.add(Dense(1024, 256))
+	model.add(Activation('tanh'))
+	#model.add(Dropout(0.5))
 
-model.add(Dense(256, 1))
-model.add(Activation('tanh'))
+	model.add(Dense(256, 1))
+	model.add(Activation('tanh'))
 
-#sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='mean_squared_error', optimizer='rmsprop')
+	model.compile(loss='mean_squared_error', optimizer='rmsprop')
+else:
+	model = pkl.load(open(args.cont,'r'))
 
 h5_file = args.f 
 r = args.r
@@ -48,11 +56,14 @@ X_test = HDF5Matrix(h5_file, 'data', test_start, test_end)
 y_train = HDF5Matrix(h5_file, 'label', train_start, train_end)
 y_test = HDF5Matrix(h5_file, 'label', test_start, test_end)
 
+history = LossHistory()
+import os
+if not os.path.isdir("./models/regression"):
+	os.mkdir("./models/regression")
 
-model.fit(X_train, y_train, batch_size=1024, nb_epoch=1, shuffle=False)
-import cPickle as pkl
-import sys
-sys.setrecursionlimit(40000)
+checkpointer = ModelCheckpoint(filepath="./models/regression/best_%s.hdf5"%args.name, verbose=1, save_best_only=True)
+model.fit(X_train, y_train, batch_size=1024, nb_epoch=args.n, shuffle=False, callbacks=[history, checkpointer])
+
 pkl.dump(model, open("model_%s.pkl"%args.name, "w"))
 
-score = model.evaluate(X_test, y_test, batch_size=1024)
+score = model.evaluate(X_test, y_test, batch_size=2048)
